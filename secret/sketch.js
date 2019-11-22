fcdde744c8e5bea2e4d69afb53ecde6e97fd38b5
epiceli <40308546+epiceli@users.noreply.github.com>
2019-11-21 20:17:55 -0600

    added secret

--- secret/sketch.js
+++ secret/sketch.js
@@ -1,15 +1,25 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Super secret thing
 
+let command = "not going to open";
+let trump;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  trump = loadImage("assets/trump.jpg");
 }
 
 function draw() {
   background(220);
+
+  if (command === "open sesame") {
+    textSize(32);
+    let message = "Secret Stuff!!!   "
+    for (let x = 0; x < width; x += textWidth(message)) {
+      for (let y = 0; y < height; y += 30) {
+        color("red");
+        text(message, x, y);
+      }
+    }
+    image(trump, 0, 0);
+  }
 }
